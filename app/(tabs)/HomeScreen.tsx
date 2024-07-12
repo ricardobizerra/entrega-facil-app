f6860b6b9ce983d59049c0ddf12e443feb91b4a3
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-07-11 23:19:02 -0300

    feat: adiciona cadastro de localização e conversão das coordenadas para PLU codes

--- app/(tabs)/HomeScreen.tsx
+++ app/(tabs)/HomeScreen.tsx
@@ -1,25 +1,102 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { useRouter, useLocalSearchParams } from 'expo-router';
+import * as Location from 'expo-location';
+import { collection, doc, getDocs, query, updateDoc, where } from 'firebase/firestore';
+import { database } from '@/config/firebaseConfig';
+import OpenLocationCode from 'open-location-code-typescript';
+import { Snackbar } from 'react-native-paper';
+
+interface User {
+  id?: string;
+  email?: string;
+  name?: string;
+  phone?: string;
+  location?: string;
+}
 
 export default function HomeScreen() {
   const router = useRouter();
   const { email, name, phone } = useLocalSearchParams();
+
+  const [user, setUser] = useState<User | undefined>({
+    email: email as string,
+    name: name as string,
+    phone: phone as string,
+  });
+  const [visible, setVisible] = useState(false);
 
   function handleLogout() {
     // Lógica para logout (pode incluir Firebase Auth signOut se necessário)
     router.push('/LoginScreen'); // Redirecionar para a tela de login após logout
   }
 
+  async function getLocation() {
+    if (!user || !user?.id) return;
+
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      console.log('Permissão de localização negada');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    // update the current user docs with the location
+    const usersRef = collection(database, 'users');
+
+    // convert the location to OpenLocationCode
+    const olc = OpenLocationCode.encode(location.coords.latitude, location.coords.longitude, 10);
+
+    if (!olc) return;
+
+    // update the user doc with the location
+    try {
+      await updateDoc(doc(usersRef, user.id), {
+        location: olc,
+      });
+      setUser((user) => ({ ...user, location: olc }));
+      setVisible(true);
+    } catch (error) {
+      console.error('Error updating document: ', error);
+    }
+  }
+
+  useEffect(() => {
+    async function fetchData() {
+      const usersRef = collection(database, 'users');
+      const q = query(usersRef, where('email', '==', email));
+      const querySnapshot = await getDocs(q);
+      
+      setUser({ ...querySnapshot.docs[0].data(), id: querySnapshot.docs[0].id } as User);
+    }
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.welcomeText}>Bem-vindo à Home!</Text>
-      <Text style={styles.userInfo}>Email: {email}</Text>
-      <Text style={styles.userInfo}>Nome: {name}</Text>
-      <Text style={styles.userInfo}>Celular: {phone}</Text>
+      <Text style={styles.userInfo}>Email: {user?.email}</Text>
+      <Text style={styles.userInfo}>Nome: {user?.name}</Text>
+      <Text style={styles.userInfo}>Celular: {user?.phone}</Text>
+      {user?.location && <Text style={[styles.userInfo, styles.userLocationInfo]}>Localização salva! Ela será utilizada para que os pedidos cheguem até você.</Text>}
+      {user && !user.location && (
+        <TouchableOpacity style={styles.locationButton} onPress={getLocation}>
+          <Text style={styles.buttonText}>Adicionar localização</Text>
+        </TouchableOpacity>
+      )}
       <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
         <Text style={styles.buttonText}>Logout</Text>
       </TouchableOpacity>
+
+      <Snackbar
+        visible={visible}
+        onDismiss={() => setVisible(false)}
+        duration={2000}
+        style={{ backgroundColor: 'green' }}
+      >
+        Localização adicionada ✅
+      </Snackbar>
     </View>
   );
 }
@@ -30,6 +107,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#f0f8ff',
+    padding: 16,
   },
   welcomeText: {
     fontSize: 24,
@@ -38,6 +116,23 @@ const styles = StyleSheet.create({
   userInfo: {
     fontSize: 18,
     marginBottom: 10,
+    textAlign: 'center',
+  },
+  userLocationInfo: { 
+    fontSize: 16,
+    marginTop: 8,
+    color: '#fff',
+    backgroundColor: 'green',
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    borderRadius: 16
+  },
+  locationButton: {
+    backgroundColor: '#1e90ff',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
   },
   logoutButton: {
     backgroundColor: '#ff6347',
