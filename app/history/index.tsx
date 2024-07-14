7cf012a77043c81d5b227576f4eda3796e723edb
Jessyca Ferreira <ferreiradasilvajessyca@gmail.com>
2024-07-14 18:50:36 -0300

    refactor: Update History component UI and add selection tabs for "Em andamento" and "Finalizado"

--- app/history/index.tsx
+++ app/history/index.tsx
@@ -2,61 +2,40 @@ import React, { useState, useEffect } from 'react';
 import { database } from '@/config/firebaseConfig';
 import { collection, getDocs, query, where, Timestamp } from 'firebase/firestore';
 import styled from 'styled-components/native';
-import { Text, View, Button, TouchableOpacity } from 'react-native';
+import { Text, View, Button, TouchableOpacity, TextInput } from 'react-native';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
-import Logo from '@/assets/images/logo-no-text.svg'; // Importando o SVG como componente
+import Logo from '@/assets/images/logo-no-text.svg';
+import SearchIcon from '@/assets/images/search-icon.svg';
 
 const HistoryContainer = styled(View)`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 0px;
+  flex: 1;
   padding: 10px;
   background-color: #f5f5f5;
-  height: 100%;
 `;
 
 const HistoryItem = styled(View)`
   background-color: #ffffff;
-  padding: 20px;
-  margin-vertical: 10px;
+  padding: 15px;
+  margin-bottom: 10px;
+  width: 95%;
+  margin-left: 20px;
   border-radius: 20px;
-  width: 700px;
-  flex-direction: row;
-  align-items: center;
 `;
 
 const HistoryText = styled(Text)`
-  color: #3A3A3A;
+  color: #3a3a3a;
   font-size: 14px;
   font-weight: 500;
-  margin-bottom: 1px;
-  display: block;
-  margin-right: 10px;
+  margin-right: 10;
 `;
 
 const HistoryTitleText = styled(Text)`
-  color: #3A3A3A;
+  color: #3a3a3a;
   font-size: 16px;
   font-weight: 700;
   margin-bottom: 4px;
-`;
-
-interface SelectionTextProps {
-  isSelected: boolean;
-}
-
-const SelectionText = styled(Text)<SelectionTextProps>`
-    margin-top: 20px;
-    color: ${({ isSelected }) => (isSelected ? '#FA4A0C' : '#9A9A9D')};
-    font-size: 14px;
-    text-decoration: none;
-    padding-bottom: 16px;
-    border-bottom-width: ${({ isSelected }) => (isSelected ? '3px' : '0')};
-    border-bottom-color: ${({ isSelected }) => (isSelected ? '#FA4A0C' : 'transparent')}; 
-    margin-right: 20px;
 `;
 
 const LogoContainer = styled(View)`
@@ -65,8 +44,8 @@ const LogoContainer = styled(View)`
 `;
 
 const StyledLogo = styled(Logo)`
-  width: 100px;
-  height: 100px;
+  width: 60px;
+  height: 60px;
   margin-right: 10px;
 `;
 
@@ -78,15 +57,49 @@ const ActionDot = styled(View)`
 `;
 
 const ActionContainer = styled(View)`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: space-between; /* Adiciona espaço entre os itens */
+  flex-direction: row;
+  align-items: center;
 `;
 
-interface ActionDotProps {
-  status: string;
+const TabsContainer = styled(View)`
+  flex-direction: row;
+  margin-left: 30px;
+  margin-bottom: 10px;
+`;
+
+interface TabTextProps {
+  selected: boolean;
 }
+
+const TabText = styled(Text)<TabTextProps>`
+  font-size: 16px;
+  font-weight: 500;
+  margin-top: 20px;
+  margin-bottom: 10px;
+  padding-bottom: 10px;
+  margin-right: 20px;
+  color: ${({ selected }) => (selected ? '#FA4A0C' : '#9A9A9D')};
+  border-bottom-width: ${({ selected }) => (selected ? '3px' : '0')};
+  border-bottom-color: ${({ selected }) => (selected ? '#FA4A0C' : 'transparent')}; 
+`;
+
+const SearchContainer = styled(View)`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center; /* Center horizontally */
+  margin-bottom: 20px;
+`;
+
+const SearchInput = styled(TextInput)`
+  background-color: #ffffff;
+  padding: 10px;
+  border-radius: 10px;
+  width: 90%;
+`;
+
+const SearchIconContainer = styled(View)`
+  margin-right: 10px;
+`;
 
 const getStatusColor = (status: string): string => {
   switch (status) {
@@ -101,17 +114,12 @@ const getStatusColor = (status: string): string => {
   }
 };
 
-interface DeliveryAction {
-  action: string;
-  timestamp: Timestamp;
-}
-
 interface PackageHistoryItem {
   id: string;
   status: string;
   client_id: string;
   creation_date: Timestamp;
-  delivery_actions: { [key: string]: DeliveryAction };
+  delivery_actions: { [key: string]: { action: string; timestamp: Timestamp } };
 }
 
 export default function History() {
@@ -120,35 +128,48 @@ export default function History() {
 
   const [packageHistory, setPackageHistory] = useState<PackageHistoryItem[]>([]);
   const [selectedTab, setSelectedTab] = useState<'Em andamento' | 'Finalizado'>('Em andamento');
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>('');
+  const [filteredOrders, setFilteredOrders] = useState<PackageHistoryItem[]>([]);
 
   const fetchHistoryFromFirebase = async () => {
     try {
-      const q = query(
-        collection(database, 'products'),
-        where('client_id', '==', client_id)
-      );
+      const q = query(collection(database, 'products'), where('client_id', '==', client_id));
       const querySnapshot = await getDocs(q);
-      const newEntries = querySnapshot.docs.map(doc => ({
+      const newEntries = querySnapshot.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
-        delivery_actions: doc.data().delivery_actions || {}
+        delivery_actions: doc.data().delivery_actions || {},
       })) as PackageHistoryItem[];
       setPackageHistory(newEntries);
+      filterOrdersByStatus(selectedTab, newEntries); // Filter initial orders by selected tab
     } catch (error) {
       console.error('Error fetching data: ', error);
     }
   };
 
   function handleBack() {
-    router.back(); 
+    router.back();
   }
 
-  const filteredOrders = packageHistory.filter(order =>
-    selectedTab === 'Em andamento' ? order.status !== 'recebido' : order.status === 'recebido'
-  );
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    const filtered = packageHistory.filter((item) =>
+      item.id.toLowerCase().includes(query.toLowerCase())
+    );
+    setFilteredOrders(filtered);
+  };
 
-  function handleOrderDetail(item_id: string) {
+  const filterOrdersByStatus = (
+    status: 'Em andamento' | 'Finalizado',
+    orders: PackageHistoryItem[]
+  ) => {
+    const filtered = orders.filter((order) =>
+      status === 'Em andamento' ? order.status !== 'recebido' : order.status === 'recebido'
+    );
+    setFilteredOrders(filtered);
+  };
+
+  const handleOrderDetail = (item_id: string) => {
     router.push({
       pathname: '/orderDetail',
       params: {
@@ -156,34 +177,55 @@ export default function History() {
         product_id: item_id,
       },
     });
-  }
+  };
 
   useEffect(() => {
     fetchHistoryFromFirebase();
   }, []);
 
-  const getLastAction = (delivery_actions: { [key: string]: DeliveryAction }) => {
+  useEffect(() => {
+    filterOrdersByStatus(selectedTab, packageHistory);
+  }, [selectedTab, packageHistory]);
+
+  const getLastAction = (delivery_actions: {
+    [key: string]: { action: string; timestamp: Timestamp };
+  }) => {
     const actions = Object.values(delivery_actions);
     if (actions.length === 0) return { action: 'Nenhuma ação disponível', timestamp: '' };
 
-    actions.sort((a, b) => b.timestamp.toDate().getTime() - a.timestamp.toDate().getTime());
+    actions.sort(
+      (a, b) => b.timestamp.toDate().getTime() - a.timestamp.toDate().getTime()
+    );
     const lastAction = actions[0];
     return {
       action: lastAction.action,
-      timestamp: format(lastAction.timestamp.toDate(), "dd/MM/yyyy HH:mm", { locale: ptBR })
+      timestamp: format(lastAction.timestamp.toDate(), "dd/MM/yyyy HH:mm", {
+        locale: ptBR,
+      }),
     };
   };
 
   return (
     <HistoryContainer>
-      <View style={{ flexDirection: 'row', marginBottom: 20 }}>
+      <TabsContainer>
         <TouchableOpacity onPress={() => setSelectedTab('Em andamento')}>
-          <SelectionText isSelected={selectedTab === 'Em andamento'}>Em andamento</SelectionText>
+          <TabText selected={selectedTab === 'Em andamento'}>Em andamento</TabText>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => setSelectedTab('Finalizado')}>
-          <SelectionText isSelected={selectedTab === 'Finalizado'}>Finalizado</SelectionText>
+          <TabText selected={selectedTab === 'Finalizado'}>Finalizado</TabText>
         </TouchableOpacity>
-      </View>
+      </TabsContainer>
+
+      <SearchContainer>
+        <SearchIconContainer>
+          <SearchIcon />
+        </SearchIconContainer>
+        <SearchInput
+          placeholder="Pesquise por um pedido"
+          value={searchQuery}
+          onChangeText={handleSearch}
+        />
+      </SearchContainer>
 
       {filteredOrders.map((item, index) => (
         <TouchableOpacity key={index} onPress={() => handleOrderDetail(item.id)}>
@@ -192,14 +234,21 @@ export default function History() {
               <StyledLogo />
               <View>
                 <HistoryTitleText>Pedido {item.id}</HistoryTitleText>
-                <HistoryText>Previsão de chegada: {format(item.creation_date.toDate(), "dd 'de' MMMM 'de' yyyy", { locale: ptBR })}</HistoryText>
+                <HistoryText>
+                  Previsão de chegada:{' '}
+                  {format(item.creation_date.toDate(), "dd 'de' MMMM 'de' yyyy", {
+                    locale: ptBR,
+                  })}
+                </HistoryText>
                 <ActionContainer>
-                  <ActionDot style={{ backgroundColor: getStatusColor(item.status.toLowerCase()) }} />
+                  <ActionDot
+                    style={{
+                      backgroundColor: getStatusColor(item.status.toLowerCase()),
+                    }}
+                  />
+                  <HistoryText>{getLastAction(item.delivery_actions).action}</HistoryText>
                   <HistoryText>
-                      {getLastAction(item.delivery_actions).action}
-                  </HistoryText>
-                  <HistoryText>
-                      {getLastAction(item.delivery_actions).timestamp}
+                    {getLastAction(item.delivery_actions).timestamp}
                   </HistoryText>
                 </ActionContainer>
               </View>
