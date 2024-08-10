873cf1a944d9cb18ce732ee2a5835d6b1ed69361
IcaroWanderley <86797083+IcaroWanderley@users.noreply.github.com>
2024-08-10 01:05:58 -0300

    inicio do botao de imprevisto

--- app/orderDetail/index.tsx
+++ app/orderDetail/index.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
+import { Modal, Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { database } from '@/config/firebaseConfig';
 import { collection, getDocs, query, where, Timestamp } from 'firebase/firestore';
 import styled from 'styled-components/native';
-import { Text, View, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { format, isToday, isTomorrow, isYesterday } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import Logo from '@/assets/images/logo-no-text.svg';
 import Back from '@/assets/images/chevron-left.svg';
 import MapView, { Marker } from 'react-native-maps';
+import { FontAwesome } from '@expo/vector-icons'; // Import para ícones
 
 const MapContainer = styled(MapView)`
   width: 100%;
@@ -30,7 +31,6 @@ const LabelText = styled(Text)`
 const ValueText = styled(Text)`
   flex: 1;
 `;
-
 
 const OrderDetailContainer = styled(View)`
   display: flex;
@@ -66,7 +66,7 @@ export const getStatusColor = (status: string): string => {
 };
 
 const StyledImage = styled(Image).attrs({
-  resizeMode: 'cover', // This ensures the image won't stretch and keeps its aspect ratio
+  resizeMode: 'cover',
   width: Dimensions.get('window').width * 0.9
 })`
   height: 200px;
@@ -90,8 +90,8 @@ const LogoImage = styled(Logo)`
 
 const OrderDetailItem = styled(View)`
   padding: 21px;
-  flex-direction: row; /* Ensure items are aligned horizontally */
-  align-items: center; /* Center items vertically */
+  flex-direction: row;
+  align-items: center;
 `;
 
 const ActionDot = styled(View)`
@@ -102,7 +102,7 @@ const ActionDot = styled(View)`
 `;
 
 const ActionTextContainer = styled(View)`
-  margin-left: 10px; /* Adjust as needed */
+  margin-left: 10px;
 `;
 
 const OrderTitleText = styled(Text)`
@@ -117,6 +117,62 @@ const OrderDetailText = styled(Text)`
   color: #333333;
   font-size: 14px;
   margin-right: 50px;
+`;
+
+const PopupButton = styled(TouchableOpacity)`
+  background-color: #4CAF50;
+  padding: 10px;
+  border-radius: 8px;
+  margin-top: 20px;
+  align-items: center;
+`;
+
+const PopupButtonText = styled(Text)`
+  color: white;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const PopupContainer = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
+
+const PopupContent = styled(View)`
+  width: 80%;
+  padding: 20px;
+  background-color: white;
+  border-radius: 10px;
+  align-items: center;
+`;
+
+const PopupCloseButton = styled(TouchableOpacity)`
+  margin-top: 20px;
+  padding: 10px 20px;
+  background-color: #FF5733;
+  border-radius: 8px;
+`;
+
+const PopupCloseButtonText = styled(Text)`
+  color: white;
+  font-size: 16px;
+`;
+
+const NotificationOption = styled(TouchableOpacity)`
+  flex-direction: row;
+  align-items: center;
+  padding: 15px;
+  background-color: #f0f0f0;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  width: 100%;
+`;
+
+const OptionText = styled(Text)`
+  font-size: 16px;
+  margin-left: 10px;
 `;
 
 interface PackageOrderDetailItem {
@@ -143,6 +199,7 @@ interface OrderDetailProps {
 
 const OrderDetail: React.FC<OrderDetailProps> = ({ client_id, product_id, closeModal }) => {
   const [packageOrderDetail, setPackageOrderDetail] = useState<PackageOrderDetailItem[]>([]);
+  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   const fetchOrderDetailFromFirebase = async () => {
     try {
@@ -154,7 +211,7 @@ const OrderDetail: React.FC<OrderDetailProps> = ({ client_id, product_id, closeM
       const newEntries = querySnapshot.docs.map(doc => ({
         id: doc.id,
         ...doc.data(),
-        delivery_actions: doc.data().delivery_actions || {} // Default to empty object if delivery_actions doesn't exist
+        delivery_actions: doc.data().delivery_actions || {}
       })) as PackageOrderDetailItem[];
       setPackageOrderDetail(newEntries.filter(entry => entry.id === product_id));
     } catch (error) {
@@ -209,7 +266,7 @@ const OrderDetail: React.FC<OrderDetailProps> = ({ client_id, product_id, closeM
 
   const getRelativeDate = (date: Date) => {
     const now = new Date();
-  
+
     if (isToday(date)) {
       return format(date, "'hoje às' HH:mm", { locale: ptBR });
     } else if (isTomorrow(date)) {
@@ -235,75 +292,120 @@ const OrderDetail: React.FC<OrderDetailProps> = ({ client_id, product_id, closeM
             <View>
               <OrderTitleText>Pedido {item.order_name}</OrderTitleText>
               <ActionContainer>
-                  <ActionDot
-                    style={{
-                      backgroundColor: getStatusColor(item.status.toLowerCase()),
-                    }}
-                  />
-                  <OrderDetailText> {item.status === 'received'
-                      ? `Finalizado`
-                      : `Em andamento`
-                  } </OrderDetailText>
+                <ActionDot
+                  style={{
+                    backgroundColor: getStatusColor(item.status.toLowerCase()),
+                  }}
+                />
+                <OrderDetailText>
+                  {item.status === 'received' ? `Finalizado` : `Em andamento`}
+                </OrderDetailText>
 
-                  <OrderDetailText>
-                    {item.status === 'received'
-                        ? `Finalizado ${getRelativeDate(item.arrival_date.toDate())}`
-                        : `Iniciado ${getRelativeDate(item.creation_date.toDate())}`
-                    }                   
-                  </OrderDetailText>
+                <OrderDetailText>
+                  {item.status === 'received'
+                    ? `Finalizado ${getRelativeDate(item.arrival_date.toDate())}`
+                    : `Iniciado ${getRelativeDate(item.creation_date.toDate())}`}
+                </OrderDetailText>
               </ActionContainer>
               <OrderTitleText>Dados do pedido</OrderTitleText>
-              <StyledImage source={{ uri: item.icon }} style={{marginBottom: 20}} />
-
-              <OrderDetailText>
-                  {getWeightText(item.weight)}                </OrderDetailText>
-              <OrderDetailText>
-                  Objeto sensível? {item.sensitive ? 'Sim' : 'Não'}
-              </OrderDetailText>
-              <View style={{marginTop: 10}}>
-              <HorizontalContainer>
-                <LabelText
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                >
-                    {item.status === 'received'
-                      ? `Pedido entregue`
-                      : `Prazo de entrega`
-                    }
-                  </LabelText>
-                  <ValueText
-                    numberOfLines={1}
-                    ellipsizeMode="tail"
-                  >
-                      {getRelativeDate(item.arrival_date.toDate())}
-                  </ValueText>
-                  </HorizontalContainer>
-                  <HorizontalContainer>
-                <LabelText>Entregar para</LabelText>
-                <ValueText>{item.client_name}</ValueText>
-              </HorizontalContainer>
-              </View>              
-              <OrderDetailText style={{ fontWeight: 'bold', marginTop: 20 }}>Endereço de entrega</OrderDetailText>
-                <OrderDetailText>{item.address}</OrderDetailText>
-                <MapContainer
-                  initialRegion={{
-                    latitude: item.location.latitude,
-                    longitude: item.location.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                  }}
-                >
-                  <Marker
-                    coordinate={{ latitude: item.location.latitude, longitude: item.location.longitude }}
-                  />
-                </MapContainer>
-
+              <StyledImage source={{ uri: item.icon }} style={{ marginTop: 10, marginBottom: 10 }} />
+              <ActionContainer>
+                <OrderDetailText>{`Enviado para ${item.client_name}`}</OrderDetailText>
+              </ActionContainer>
+              <ActionContainer>
+                <OrderDetailText>{`Endereço: ${item.address}`}</OrderDetailText>
+              </ActionContainer>
+              <ActionContainer>
+                <OrderDetailText>{getWeightText(item.weight)}</OrderDetailText>
+              </ActionContainer>
+              {item.sensitive && (
+                <ActionContainer>
+                  <OrderDetailText>{`Conteúdo Sensível`}</OrderDetailText>
+                </ActionContainer>
+              )}
+              <ActionContainer>
+                <OrderDetailText>{`Última ação: ${getLastAction(item.delivery_actions).action} em ${getLastAction(item.delivery_actions).timestamp
+                  }`}</OrderDetailText>
+              </ActionContainer>
             </View>
           </OrderDetailIcon>
+
+          {/* Botão para abrir o Popup */}
+          <PopupButton onPress={() => setIsPopupVisible(true)}>
+            <PopupButtonText>Notificar Imprevisto</PopupButtonText>
+          </PopupButton>
+
+          {/* Mapa */}
+          <MapContainer
+            initialRegion={{
+              latitude: item.location.latitude,
+              longitude: item.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
+            }}
+          >
+            <Marker
+              coordinate={{ latitude: item.location.latitude, longitude: item.location.longitude }}
+            />
+          </MapContainer>
         </View>
       ))}
+
+      {/* Modal Popup */}
+      <Modal
+        visible={isPopupVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setIsPopupVisible(false)}
+      >
+        <PopupContainer>
+          <PopupContent>
+            <Text style={{ fontSize: 18, fontWeight: 'bold', marginBottom: 10 }}>
+              Notificar Improvisto
+            </Text>
+            <Text style={{ marginBottom: 20 }}>
+              Nos informe se algo tiver ocorrido ao longo dessa entrega.
+            </Text>
+
+            {/* Opções de Notificação */}
+            <NotificationOption>
+              <FontAwesome name="clock-o" size={24} color="black" />
+              <OptionText>Atraso no trânsito</OptionText>
+            </NotificationOption>
+
+            <NotificationOption>
+              <FontAwesome name="user-times" size={24} color="black" />
+              <OptionText>Responsável não encontrado no local de entrega</OptionText>
+            </NotificationOption>
+
+            <NotificationOption>
+              <FontAwesome name="map-marker" size={24} color="black" />
+              <OptionText>Endereço incorreto</OptionText>
+            </NotificationOption>
+
+            <NotificationOption>
+              <FontAwesome name="car" size={24} color="black" />
+              <OptionText>Pedido sofreu acidente no percurso</OptionText>
+            </NotificationOption>
+
+            <NotificationOption>
+              <FontAwesome name="ambulance" size={24} color="black" />
+              <OptionText>Entregador sofreu acidente no percurso</OptionText>
+            </NotificationOption>
+
+            <NotificationOption>
+              <FontAwesome name="ellipsis-h" size={24} color="black" />
+              <OptionText>Outro</OptionText>
+            </NotificationOption>
+
+            <PopupCloseButton onPress={() => setIsPopupVisible(false)}>
+              <PopupCloseButtonText>Fechar</PopupCloseButtonText>
+            </PopupCloseButton>
+          </PopupContent>
+        </PopupContainer>
+      </Modal>
     </OrderDetailContainer>
   );
-}
+};
 
 export default OrderDetail;
