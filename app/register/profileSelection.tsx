f3a4a89eb9ab3e94d0ea7bb61f7d7acb34715e16
ricardobizerra <ricardo.bizerra19@gmail.com>
2024-08-09 23:34:23 -0300

    feat: add selection logic & redirection to last step

--- app/register/profileSelection.tsx
+++ app/register/profileSelection.tsx
@@ -2,9 +2,18 @@ import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import ArrowLeft from '@/assets/images/ArrowLeft.svg';
 import DeliveryProfile from '@/assets/images/register/DeliveryProfile.svg';
 import { useRouter } from "expo-router";
+import { useState } from "react";
+
+type ProfileEnum = 'entregador' | 'armazenador' | 'entregador-armazenador';
 
 export default function ProfileSelection() {
   const router = useRouter();
+
+  const [selection, setSelection] = useState<ProfileEnum | undefined>(undefined);
+
+  const handleSelection = (selection: ProfileEnum) => {
+    setSelection(selection);
+  }
 
   return (
     <View style={styles.container}>
@@ -18,11 +27,17 @@ export default function ProfileSelection() {
       </View>
 
       <View style={styles.selectionContainer}>
-        <TouchableOpacity style={styles.selection} onPress={() => {}}>
+        <TouchableOpacity
+          style={[styles.selection, selection === 'entregador' && styles.selectionActive]}
+          onPress={() => handleSelection('entregador')}
+        >
           <Image source={require('@/assets/images/register/DeliveryPerson.png')} style={styles.selectionImage} />
           <View style={styles.selectionText}>
             <Text style={styles.selectionTitle}>
-              Quero ser entregador
+              Quero ser{" "}
+              <Text style={styles.selectionTitleBold}>
+                entregador
+              </Text>
             </Text>
             <Text style={styles.selectionDescription}>
               Entregue encomendas para sua comunidade
@@ -30,11 +45,17 @@ export default function ProfileSelection() {
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.selection} onPress={() => {}}>
+        <TouchableOpacity
+          style={[styles.selection, selection === 'armazenador' && styles.selectionActive]}
+          onPress={() => handleSelection('armazenador')}
+        >
           <Image source={require('@/assets/images/register/HolderPerson.png')} style={styles.selectionImage} />
           <View style={styles.selectionText}>
             <Text style={styles.selectionTitle}>
-              Quero ser armazenador
+              Quero ser{" "}
+              <Text style={styles.selectionTitleBold}>
+                armazenador
+              </Text>
             </Text>
             <Text style={styles.selectionDescription}>
               Armazene encomendas de sua comunidade em um espaço de sua residência
@@ -42,16 +63,45 @@ export default function ProfileSelection() {
           </View>
         </TouchableOpacity>
 
-        <TouchableOpacity style={styles.selection} onPress={() => {}}>
+        <TouchableOpacity
+          style={[styles.selection, selection === 'entregador-armazenador' && styles.selectionActive]}
+          onPress={() => handleSelection('entregador-armazenador')}
+        >
           <Image source={require('@/assets/images/register/DeliveryHolderPerson.png')} style={styles.selectionImage} />
           <View style={styles.selectionText}>
             <Text style={styles.selectionTitle}>
-              Quero ser entregador e armazenador
+              Quero ser{" "}
+              <Text style={styles.selectionTitleBold}>
+                entregador
+              </Text>
+              {" e "}
+              <Text style={styles.selectionTitleBold}>
+                armazenador
+              </Text>
             </Text>
             <Text style={styles.selectionDescription}>
               Atue nas duas modalidades do Tá Entregue
             </Text>
           </View>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            if (!selection) return;
+            
+            router.push({ 
+              pathname: '/register/info',
+              params: { kind: selection === 'entregador-armazenador' ? ['entregador', 'armazenador'] : [selection] }
+            });
+          }}
+          style={[styles.nextButton, !selection && styles.nextButtonDisabled]}
+          disabled={!selection}
+        >
+          <Text style={styles.nextButtonText}>
+            {!selection ? 'Selecione um perfil' : 'Próximo'}
+          </Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -89,29 +139,59 @@ const styles = StyleSheet.create({
   selection: {
     width: '100%',
     borderRadius: 20,
-    borderWidth: 1,
-    borderColor: '#ccc',
+    borderWidth: 2,
+    borderColor: '#f2f2f2',
     height: 120,
     overflow: 'hidden',
     backgroundColor: '#f2f2f2',
     flexDirection: 'row',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  selectionActive: {
+    borderColor: '#ffa500',
   },
   selectionImage: {
-    width: '50%',
+    width: '40%',
     height: '100%',
   },
   selectionText: {
-    width: '50%',
+    width: '56%',
     justifyContent: 'center',
-    marginLeft: 8,
+    marginVertical: '2%',
   },
   selectionTitle: {
     fontSize: 16,
-    fontWeight: '700',
     color: '#000',
+  },
+  selectionTitleBold: {
+    fontWeight: 'bold',
   },
   selectionDescription: {
     fontSize: 12,
     color: '#000',
+  },
+  nextButton: {
+    backgroundColor: '#ffa500',
+    padding: 16,
+    borderRadius: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 16,
+    width: 'auto',
+  },
+  nextButtonDisabled: {
+    backgroundColor: '#ccc',
+  },
+  nextButtonText: {
+    color: '#000',
+    fontSize: 16,
+    fontWeight: '700',
   },
 });
